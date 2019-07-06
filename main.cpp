468bc5f32faee2a79cef2ffc0987faf626640438
backskin <vbackskin@gmail.com>
2019-07-06 14:00:39 +0400

    v0.3.6

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
-
     MyServer* server = new MyServer();
-    server->show();
+    server -> show();
     return app.exec();
 }
